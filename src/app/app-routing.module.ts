27ff44b2e6cc3ae7c591080bc402bb6684751f59
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 13:58:00 -0700

    Add main route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { LayoutComponent } from './layout/layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

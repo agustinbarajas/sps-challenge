fe85e3f46383d6e589fb959a86ca93a986db50c6
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 20:46:51 -0700

    Add star rate icon pipe

--- src/app/pipes/star-rate-icon/star-rate-icon.pipe.ts
+++ src/app/pipes/star-rate-icon/star-rate-icon.pipe.ts
@@ -1,10 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { STAR } from 'src/app/constants/star.const';
+import {
+  HALF_STAR_COUNT,
+  STAR_BORDER,
+  STAR_HALF,
+} from './../../constants/star.const';
+
 @Pipe({
   name: 'starRateIcon',
 })
 export class StarRateIconPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(rate: number, currentStar: number): string {
+    const decimal = Math.abs(rate) - Math.floor(rate);
+
+    if (rate >= currentStar) {
+      return STAR;
+    }
+
+    if (Math.ceil(rate) === currentStar && decimal > HALF_STAR_COUNT) {
+      return STAR_HALF;
+    }
+
+    return STAR_BORDER;
   }
 }

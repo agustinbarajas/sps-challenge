8c5f538dc9c99b4304d8cfdf2c7dacc0b08cfa79
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 20:59:33 -0700

    Add product catalog list

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Product } from 'src/app/interfaces/product.interface';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss'],
 })
-export class CatalogComponent {}
+export class CatalogComponent implements OnInit {
+  products$!: Observable<Product[]>;
+
+  constructor(private readonly productService: ProductService) {}
+
+  ngOnInit() {
+    this.products$ = this.productService.getProducts();
+  }
+
+  trackProduct(index: number, product: Product): number {
+    return product.id;
+  }
+}

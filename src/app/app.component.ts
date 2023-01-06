1844bea575db7b11dcfc813eaa81a5a119b41665
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 23:44:21 -0700

    Add logic to detect login when reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UserService } from './services/user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private readonly userService: UserService) {}
+
+  ngOnInit(): void {
+    const hasLogin = this.userService.hasToken();
+    this.userService.hasSession$.next(hasLogin);
+  }
+}

1dfa9f864625db7b6ed244228d6f49c69137120c
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 20:58:50 -0700

    Add service to get products

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,6 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Product } from './../../interfaces/product.interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>('https://fakestoreapi.com/products');
+  }
+}

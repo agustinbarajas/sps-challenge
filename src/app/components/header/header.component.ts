89f23914cf4d8d2b2df9d4cce141c75d7fe8c875
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 23:42:54 -0700

    Add user logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { LOGIN_ROUTE } from './../../constants/routes.cont';
+import { UserService } from './../../services/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -12,9 +13,16 @@ export class HeaderComponent {
   hasLogin = false;
   cartItems = 0;
 
-  constructor(private readonly router: Router) {}
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService
+  ) {}
 
   goLogin() {
     this.router.navigate([LOGIN_ROUTE]);
+  }
+
+  logout() {
+    this.userService.logout();
   }
 }

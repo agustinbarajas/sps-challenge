d4c0487b1f7e2c99206731c9a49cd91344a0b5ab
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 23:43:31 -0700

    Add subscription to detect login or logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 import { LOGIN_ROUTE } from './../../constants/routes.cont';
 import { UserService } from './../../services/user/user.service';
@@ -9,14 +10,22 @@ import { UserService } from './../../services/user/user.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
   hasLogin = false;
   cartItems = 0;
+
+  private readonly unsubscribe$ = new Subject();
 
   constructor(
     private readonly router: Router,
     private readonly userService: UserService
-  ) {}
+  ) {
+    this.userService.hasSession$
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((hasLogin: boolean) => {
+        this.hasLogin = hasLogin;
+      });
+  }
 
   goLogin() {
     this.router.navigate([LOGIN_ROUTE]);
@@ -24,5 +33,10 @@ export class HeaderComponent {
 
   logout() {
     this.userService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.complete();
   }
 }

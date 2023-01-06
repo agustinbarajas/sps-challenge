0db6bc429d32ba766c97f1c91da25ae7ff6fe287
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 21:15:50 -0700

    Add login route

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LOGIN_ROUTE, PRODUCT_CATALOG_ROUTE } from './../constants/routes.cont';
 import { CatalogComponent } from './catalog/catalog.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: CatalogComponent
-  }
+    path: PRODUCT_CATALOG_ROUTE,
+    component: CatalogComponent,
+  },
+  {
+    path: LOGIN_ROUTE,
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {}

48757ec7658a5b119aa5f40576ffc75b1624cc4f
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 17:32:00 -0700

    Add header design

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
@@ -9,6 +14,10 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
   ],
   exports: [
     HeaderComponent,

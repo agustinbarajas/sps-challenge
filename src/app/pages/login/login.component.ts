d6afcf4034c55bfac811a8e92ce3b108a1d5b250
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 23:40:06 -0700

    Add user login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+
+import { PRODUCT_CATALOG_ROUTE } from './../../constants/routes.cont';
+import { UserService } from './../../services/user/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
   loginForm: FormGroup;
+  private readonly unsubscribe$ = new Subject();
 
-  constructor(private readonly fb: FormBuilder) {
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly userService: UserService,
+    private readonly router: Router
+  ) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
     });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+
+      this.userService
+        .login(username, password)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(() => {
+          this.router.navigate([PRODUCT_CATALOG_ROUTE], { replaceUrl: true });
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.complete();
   }
 }

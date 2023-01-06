15adb93480537ff30a3444b6128c9f71fad4ee1c
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 23:39:17 -0700

    Add user service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,8 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
+  private _hasSession$ = new BehaviorSubject<boolean>(false);
+  private prefixApp = '--ew-';
+  private tokenKey = `${this.prefixApp}token`;
+
+  constructor(private readonly http: HttpClient) {}
+
+  get hasSession$(): BehaviorSubject<boolean> {
+    return this._hasSession$;
+  }
+
+  login(username: string, password: string): Observable<{ token: string }> {
+    return this.http
+      .post<{ token: string }>('https://fakestoreapi.com/auth/login', {
+        username,
+        password,
+      })
+      .pipe(
+        tap(({ token }) => {
+          localStorage.setItem(this.tokenKey, btoa(token));
+          this.hasSession$.next(true);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem(this.tokenKey);
+    this.hasSession$.next(false);
+  }
+
+  hasToken(): boolean {
+    return !!localStorage.getItem(this.tokenKey);
+  }
 }

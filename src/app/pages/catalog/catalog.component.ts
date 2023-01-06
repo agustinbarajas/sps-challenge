83e4134dda1e81971c965bdd1ddd5074b3c7c46d
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-06 00:56:55 -0700

    Add filter by category

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 import { Product } from 'src/app/interfaces/product.interface';
 import { ProductService } from 'src/app/services/product/product.service';
@@ -16,6 +16,8 @@ export class CatalogComponent implements OnInit {
   categories$!: Observable<string[]>;
   categoryControl = new FormControl('');
 
+  private readonly unsubscribe$ = new Subject();
+
   constructor(
     private readonly productService: ProductService,
     private readonly categoriesService: CategoriesService
@@ -24,6 +26,16 @@ export class CatalogComponent implements OnInit {
   ngOnInit() {
     this.products$ = this.productService.getProducts();
     this.categories$ = this.categoriesService.getCategories();
+
+    this.categoryControl.valueChanges
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((category: string | null) => {
+        if (!category) {
+          this.products$ = this.productService.getProducts();
+        } else {
+          this.products$ = this.productService.getProducts(category);
+        }
+      });
   }
 
   trackProduct(index: number, product: Product): number {

9f9589a168982c329a55e40027f35b8735334e9e
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-05 21:16:18 -0700

    Add navigation to login route

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LOGIN_ROUTE } from './../../constants/routes.cont';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   hasLogin = false;
   cartItems = 0;
+
+  constructor(private readonly router: Router) {}
+
+  goLogin() {
+    this.router.navigate([LOGIN_ROUTE]);
+  }
 }

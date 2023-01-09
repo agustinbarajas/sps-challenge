d6bc901754969893e5a7965ecb74dc03229687bf
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-08 19:55:03 -0700

    Add token decode to get userId

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
@@ -9,11 +10,19 @@ export class UserService {
   private _hasSession$ = new BehaviorSubject<boolean>(false);
   private prefixApp = '--ew-';
   private tokenKey = `${this.prefixApp}token`;
+  private _userId: number | null = null;
 
   constructor(private readonly http: HttpClient) {}
 
   get hasSession$(): BehaviorSubject<boolean> {
     return this._hasSession$;
+  }
+
+  get userId(): number | null {
+    if (!this._userId) {
+      this.assignUserId();
+    }
+    return this._userId;
   }
 
   login(username: string, password: string): Observable<{ token: string }> {
@@ -25,6 +34,7 @@ export class UserService {
       .pipe(
         tap(({ token }) => {
           localStorage.setItem(this.tokenKey, btoa(token));
+          this._userId = +(this.decodeToken(token)?.sub as string);
           this.hasSession$.next(true);
         })
       );
@@ -32,10 +42,25 @@ export class UserService {
 
   logout() {
     localStorage.removeItem(this.tokenKey);
+    this._userId = null;
     this.hasSession$.next(false);
   }
 
   hasToken(): boolean {
-    return !!localStorage.getItem(this.tokenKey);
+    const token = localStorage.getItem(this.tokenKey);
+    if (token) {
+      this.assignUserId();
+    }
+    return !!token;
+  }
+
+  private decodeToken(token: string): JwtPayload {
+    return jwtDecode<JwtPayload>(token);
+  }
+
+  private assignUserId(): boolean {
+    const token = localStorage.getItem(this.tokenKey) as string;
+    this._userId = +(this.decodeToken(token)?.sub as string);
+    return !!this._userId;
   }
 }

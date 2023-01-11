42fd8edf6afb9b7980615d411a17e32c4fcb5756
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-11 12:11:00 -0700

    Add pipe to get product

--- src/app/pipes/get-product/get-product.pipe.ts
+++ src/app/pipes/get-product/get-product.pipe.ts
@@ -1,10 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ProductService } from 'src/app/services/product/product.service';
+import { Product } from './../../interfaces/product.interface';
 
 @Pipe({
   name: 'getProduct',
 })
 export class GetProductPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  constructor(private readonly productService: ProductService) {}
+
+  transform(productId: number): Observable<Product> {
+    return this.productService.getProduct(productId);
   }
 }

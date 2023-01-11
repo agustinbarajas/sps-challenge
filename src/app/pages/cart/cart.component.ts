3678f3ccdcc5ef489aa3e5c37ece1859147238f9
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-11 12:13:23 -0700

    Add get cart observable

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Cart } from 'src/app/interfaces/cart.interface';
+import { CartService } from './../../services/cart/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent {}
+export class CartComponent implements OnInit {
+  cart$!: Observable<Cart>;
+
+  constructor(private readonly cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cart$ = this.cartService.getCart();
+  }
+}

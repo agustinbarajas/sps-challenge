0b07364f823954da68ce4ee922607a807ff7e777
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-11 12:56:33 -0700

    Add cart product design

--- src/app/components/cart-product/cart-product.component.ts
+++ src/app/components/cart-product/cart-product.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Product } from './../../interfaces/product.interface';
 
 @Component({
   selector: 'app-cart-product',
   templateUrl: './cart-product.component.html',
   styleUrls: ['./cart-product.component.scss'],
 })
-export class CartProductComponent {}
+export class CartProductComponent {
+  @Input() product!: Product;
+}

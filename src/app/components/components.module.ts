6cc994f273f62eda2734d001a105df540afe58d8
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-01-11 12:18:44 -0700

    Create cart product component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { PipesModule } from '../pipes/pipes.module';
+import { CartProductComponent } from './cart-product/cart-product.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductComponent } from './product/product.component';
 import { StarRateComponent } from './star-rate/star-rate.component';
-import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
-  declarations: [HeaderComponent, ProductComponent, StarRateComponent],
+  declarations: [
+    HeaderComponent,
+    ProductComponent,
+    StarRateComponent,
+    CartProductComponent,
+  ],
   imports: [
     CommonModule,
     MatToolbarModule,
@@ -27,6 +33,11 @@ import { PipesModule } from '../pipes/pipes.module';
     MatDividerModule,
     PipesModule,
   ],
-  exports: [HeaderComponent, ProductComponent, StarRateComponent],
+  exports: [
+    HeaderComponent,
+    ProductComponent,
+    StarRateComponent,
+    CartProductComponent,
+  ],
 })
 export class ComponentsModule {}
